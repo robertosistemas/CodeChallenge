28a570fe287f3e05cb2a5a9122273f51aa869cb2
Roberto Carlos <roberto.silva-terceiro@cea.com.br>
2022-10-18 11:08:44 -0300

    Remove modificador public

--- src/CodeChallenge.Domain/Abstractions/IPaged.cs
+++ src/CodeChallenge.Domain/Abstractions/IPaged.cs
@@ -3,7 +3,7 @@
     public interface IPaged
     {
         int PageNumber { get; set; }
-        public int PageSize { get; set; }
+        int PageSize { get; set; }
         void Normalize();
     }
 }
